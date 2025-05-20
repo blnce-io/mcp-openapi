233cd7dfe7c3d4c001961a544b6657d5997fd730
Ivan Rubinson <soryy708@gmail.com>
2025-05-20 17:41:28 +0300

    copy openapi specs between dirs before starting

--- src/http-server.ts
+++ src/http-server.ts
@@ -1,9 +1,13 @@
 import { bootstrapHttpServer } from './infra/http-server';
 import * as logger from './infra/logger';
 import { initMcpServer } from './mcp-server';
+import { populateSpecsDirectory } from './populate-specs-dir';
 
-initMcpServer();
-bootstrapHttpServer().catch((error) => {
+(async () => {
+  await populateSpecsDirectory();
+  initMcpServer();
+  await bootstrapHttpServer();
+})().catch((error) => {
   logger.error(error);
   process.exit(1);
 });
--- src/infra/config.ts
+++ src/infra/config.ts
@@ -1,4 +1,4 @@
-type ConfigKey = 'PORT' | 'OPENAPI_SPECS_DIRECTORY';
+type ConfigKey = 'PORT' | 'OPENAPI_SPECS_DIRECTORY' | 'OPENAPI_SPECS_SOURCE_DIRECTORY';
 
 export function getConfig(key: ConfigKey): string | undefined {
   return process.env[key];
