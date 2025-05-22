fadf276a4788d6bb366c1d852486d3d9d03aba85
Ivan Rubinson <soryy708@gmail.com>
2025-05-22 10:28:06 +0300

    add most request headers to context

--- src/infra/request-logger.ts
+++ src/infra/request-logger.ts
@@ -2,6 +2,14 @@ import type { Request, Response, NextFunction } from 'express';
 import * as logger from './logger';
 
 export function requestLogger(request: Request, response: Response, next: NextFunction): void {
-  logger.log('Request start', { method: request.method, url: request.url });
+  logger.log('Request start', {
+    method: request.method,
+    url: request.url,
+    headers: omit(request.headers, ['cookie', 'authorization']),
+  });
   next();
+}
+
+function omit(object: Record<string, unknown>, keys: string[]): Record<string, unknown> {
+  return Object.fromEntries(Object.entries(object).filter(([key]) => !keys.includes(key.toLowerCase())));
 }
