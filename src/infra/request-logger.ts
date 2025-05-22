c52d8e6f53c2a8780bb58c8e0c5f081394227a07
Ivan Rubinson <soryy708@gmail.com>
2025-05-22 13:45:38 +0300

    dont log requests from kube-probe

--- src/infra/request-logger.ts
+++ src/infra/request-logger.ts
@@ -2,11 +2,13 @@ import type { Request, Response, NextFunction } from 'express';
 import * as logger from './logger';
 
 export function requestLogger(request: Request, response: Response, next: NextFunction): void {
-  logger.log('Request start', {
-    method: request.method,
-    url: request.url,
-    headers: omit(request.headers, ['cookie', 'authorization']),
-  });
+  if (!request.headers['user-agent'].startsWith('kube-probe/')) {
+    logger.log('Request start', {
+      method: request.method,
+      url: request.url,
+      headers: omit(request.headers, ['cookie', 'authorization']),
+    });
+  }
   next();
 }
 
